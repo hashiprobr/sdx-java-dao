0d08f3527e7f088cd805d0e1aecbd9f951e42fee
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-12-20 15:02:04 -0300

    Refactor HandleFactory class

--- src/main/java/br/pro/hashi/sdx/dao/reflection/HandleFactory.java
+++ src/main/java/br/pro/hashi/sdx/dao/reflection/HandleFactory.java
@@ -1,28 +1,33 @@
+/*
+ * Copyright (c) 2023 Marcelo Hashimoto
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 package br.pro.hashi.sdx.dao.reflection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class HandleFactory {
-	private static final HandleFactory INSTANCE = new HandleFactory();
+    private static final HandleFactory INSTANCE = new HandleFactory();
 
-	public static HandleFactory getInstance() {
-		return INSTANCE;
-	}
+    public static HandleFactory getInstance() {
+        return INSTANCE;
+    }
 
-	private final Map<Class<?>, Handle<?>> cache;
+    private final ConcurrentMap<Class<?>, Handle<?>> cache;
 
-	HandleFactory() {
-		this.cache = new HashMap<>();
-	}
+    HandleFactory() {
+        this.cache = new ConcurrentHashMap<>();
+    }
 
-	public <E> Handle<E> get(Class<E> type) {
-		@SuppressWarnings("unchecked")
-		Handle<E> handle = (Handle<E>) cache.get(type);
-		if (handle == null) {
-			handle = Handle.newInstance(type);
-			cache.put(type, handle);
-		}
-		return handle;
-	}
+    @SuppressWarnings("unchecked")
+    public <E> Handle<E> get(Class<E> type) {
+        return (Handle<E>) cache.computeIfAbsent(type, Handle::newInstance);
+    }
 }
--- src/test/java/br/pro/hashi/sdx/dao/reflection/HandleFactoryTest.java
+++ src/test/java/br/pro/hashi/sdx/dao/reflection/HandleFactoryTest.java
@@ -1,10 +1,14 @@
-package br.pro.hashi.sdx.dao.reflection;
+/*
+ * Copyright (c) 2023 Marcelo Hashimoto
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
+package br.pro.hashi.sdx.dao.reflection;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,36 +16,38 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
 class HandleFactoryTest {
-	private AutoCloseable mocks;
-	private HandleFactory f;
+    private AutoCloseable mocks;
+    private HandleFactory f;
 
-	@BeforeEach
-	void setUp() {
-		mocks = MockitoAnnotations.openMocks(this);
+    @BeforeEach
+    void setUp() {
+        mocks = MockitoAnnotations.openMocks(this);
 
-		f = new HandleFactory();
-	}
+        f = new HandleFactory();
+    }
 
-	@AfterEach
-	void tearDown() {
-		assertDoesNotThrow(() -> {
-			mocks.close();
-		});
-	}
+    @AfterEach
+    void tearDown() {
+        assertDoesNotThrow(() -> mocks.close());
+    }
 
-	@Test
-	void getsInstance() {
-		assertInstanceOf(HandleFactory.class, HandleFactory.getInstance());
-	}
+    @Test
+    void getsInstance() {
+        assertInstanceOf(HandleFactory.class, HandleFactory.getInstance());
+    }
 
-	@Test
-	void gets() {
-		@SuppressWarnings("rawtypes")
-		MockedStatic<Handle> handleStatic = mockStatic(Handle.class);
-		handleStatic.when(() -> Handle.newInstance(Object.class)).thenReturn(mock(Handle.class));
-		Handle<Object> handle = f.get(Object.class);
-		assertSame(handle, f.get(Object.class));
-		handleStatic.close();
-	}
+    @Test
+    void gets() {
+        @SuppressWarnings("rawtypes")
+        MockedStatic<Handle> handleStatic = mockStatic(Handle.class);
+        handleStatic.when(() -> Handle.newInstance(Object.class)).thenReturn(mock(Handle.class));
+        Handle<Object> handle = f.get(Object.class);
+        handleStatic.close();
+        assertSame(handle, f.get(Object.class));
+    }
 }
